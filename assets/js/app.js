a6cdfe4793bc2d02043d6da38c7bc2032cdb077c
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-02 13:24:12 -0800

    extracting value out of dropdown menu

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$("#state-select").on("click", function(event){
+	var selected = document.getElementById('state').value;
+	console.log(selected);
+
+	// calls function to display info from NPS
+});
+
+
+function displayInfo() {
+	
+}
