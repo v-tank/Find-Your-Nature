4f90766ea0fe56e5cb91393c5770e6cc7794f9cd
Константин Тихонов <tikhonoff@gmail.com>
2018-02-04 10:22:34 -0800

    js file modified

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,3 @@
-
-
-$(document).ready(function(){
-            $(function (){
-                $.ajax({
-                    url: 'https://developer.nps.gov/api/v1/parks', // url checked ...// error 400 not found url
-                    data: { 
-                      stateCode:'ca',
-                        api_key:'dR9liF6s3ztufwHduTKv4mfNqrtq3iGWp8dxjzcr'
-                    },
-                    dataType: 'json',
-                    success: function(response)
-                    {
-
-
-
 var designation;
 var directionsInfo;
 var directionsUrl;
@@ -26,141 +10,152 @@ var states;
 var url;
 var weatherInfo;
 
-
-var results = response.data;
 var uluru;
 var parks =[];
-//debugger;
-        for (var i = 0; i < 10; i++) {
-                console.log("name : "+i+""+ results[i].name) ;
-               // console.log("weatherInfo "+results[i].latLong) ;
-
-                if (results[i].latLong.length>0){
-
-                   
-
-                    var newMap = $("<div>").addClass('map').attr('id', 'id_'+i);
-                    var contdiv= $("<div>").addClass('contdiv');
-                    $("<p>").text(results[i].fullName).appendTo(contdiv);
-                    newMap.appendTo(contdiv);
-                    contdiv.appendTo("body");
-                    uluru = getLatLngFromString(results[i].latLong);
-                    
-
-                    parks.push([results[i].fullName, uluru]);
+var state;
+var stateCode; 
 
 
-          initMap(uluru,newMap,8) ;
-          
-        }
 
-        }
+$(document).ready(function(){
+onPageLoad();
 
-      for (var i = 0; i < parks.length; i++) {
+})
 
-      
-        var dpItem = $("<a>").addClass('dropdown-item').attr('id', '#'+i).data('geo-value',parks[i][1]);
-        dpItem.text(parks[i][0]);
-        dpItem.appendTo("#dm");
 
-        //debugger;
+$('#state').change(function(){ 
 
-     }
+  $("a.dropdown-item").remove();
+  parks=[];
+  stateCode = $(this).val();
+  state =  $("#state option:selected").data('geo');
+  onPageLoad();
 
-     foo(parks, "map", 8);
+});
 
 
 
 
-
-            function initMap(uluru, newMap, zooom) {
-
+function getStateLatLng (location){
 
 
-
-              console.log(uluru+"   "+newMap[0]);
-           
-                var map = new google.maps.Map(newMap[0]
-        , {//document.getElementById('map')
-                  zoom: zooom,
-                  center: uluru
-                });
-
-                var marker = new google.maps.Marker({
-                  position: uluru,
-                  map: map
-                });
-              }
-
-              function getLatLngFromString(ll) {
-                    var newstr = ll.replace(/lat/, '"lat"').replace(/long/i, '"lng"');
-                 
-              //  return new google.maps.LatLng( JSON.parse("{"+newstr+"}")); 
-
-              return JSON.parse("{"+newstr+"}"); 
-
-            
-          }
-
-
-
-          
-
-            $(document).on("click", ".dropdown-item", function() {
-
-                console.log($(this).data("geo-value"))
-                initMap($(this).data("geo-value"), $("#map2"), 8);
-            //  console.log(this.attr("geo-value"))
-            });
-
-  
-            
-
-
-              function foo(foo_parks, div){
-
-  // debugger;
-
-        var map = new google.maps.Map(document.getElementById(div), {
-        zoom: 5,
-        center: new google.maps.LatLng( 37.82676234, -122.4230206),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-
-        var infowindow = new google.maps.InfoWindow();
-
-        var marker, i;
-
-        for (i = 0; i < foo_parks.length; i++) {  
-
-        marker = new google.maps.Marker({
-        position: new google.maps.LatLng(foo_parks[i][1]),
-        map: map
-         });
-          }
+var geocoder = new google.maps.Geocoder();
+geocoder.geocode( { 'address': location }, function(results, status) {
+  if (status == google.maps.GeocoderStatus.OK) {
+    var templatlng = results[0].geometry.location;
+    var templatlngObj = {lat: templatlng.lat(), lng: templatlng.lng()};
+    return JSON.parse(templatlngObj)
+  } else {
+      alert("Could not find location: " + location);
+  }
+});}
 
 
 
 
+$(document).on("click", ".dropdown-item", function() {
+    mainMapInit([["",$(this).data("geo-value")]], "map", 10, [$(this).data("geo-value")]);
+waqiMapInit();
 
 
 
+});
 
 
-              }
+function mainMapInit(parks, div, zooom, center){
+
+  var centerLatLng;
+  centerLatLng = new google.maps.LatLng(center[0]);
+  var map = new google.maps.Map(document.getElementById(div), {
+  zoom: zooom,
+  center: centerLatLng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker, i;
+
+  for (i = 0; i < parks.length; i++) {  
+  marker = new google.maps.Marker({
+  position: new google.maps.LatLng(parks[i][1]),
+  map: map});
+  }
+} 
 
 
 
+function onPageLoad(){
+   $(function (){
+ 
+
+                $.ajax({
+                    url: 'https://developer.nps.gov/api/v1/parks', // url checked ...// error 400 not found url
+                    data: { 
+                    stateCode : stateCode,
+                  //  limit: 2,
+                     api_key:'dR9liF6s3ztufwHduTKv4mfNqrtq3iGWp8dxjzcr'
+                    },
+                    dataType: 'json',
+                    success: function(response)
+                    {      
+
+                     waqiMapInit();
+
+                            var results = response.data;
+                            for (var i = 0; i < results.length; i++) {
+                                    console.log("name : "+i+""+ results[i].name) ;
+                                    if (results[i].latLong.length>0){
+                                        uluru = getLatLngFromString(results[i].latLong);               
+                                        parks.push([results[i].fullName, uluru]);
+                                     }
+                            }
 
 
+                          for (var i = 0; i < parks.length; i++) {
+                              var dpItem = $("<a>").addClass('dropdown-item').attr('id', '#'+i).data('geo-value',parks[i][1]);
+                              dpItem.text(parks[i][0]);
+                              dpItem.appendTo("#dm");
+                           }
 
 
+                          if (state)  {
+                          mainMapInit(parks, "map", 6, [state]);
+                           } else{
+                          mainMapInit(parks, "map", 3, [{lat: 39.5, lng: -98.35}]);
+                          }  
+
+
+                         function getLatLngFromString(ll) {
+                                var newstr = ll.replace(/lat/, '"lat"').replace(/long/i, '"lng"');
+                                return JSON.parse("{"+newstr+"}"); 
+                          }                          
 
 
 
                     }
-                });
-            });
+                });  
+})
+}
 
-        });
 
+
+function waqiMapInit(){
+
+  // debugger;
+
+      var  map  =  new  google.maps.Map(document.getElementById('map2'),  {  
+                  center:  new  google.maps.LatLng( state),  
+                  mapTypeId:  google.maps.MapTypeId.ROADMAP,  
+                  zoom:  6  
+              });  
+  
+                              var  t  =  new  Date().getTime();  
+      var  waqiMapOverlay  =  new  google.maps.ImageMapType({  
+                  getTileUrl:  function(coord,  zoom)  {  
+                            return  'https://tiles.waqi.info/tiles/usepa-aqi/'  +  zoom  +  "/"  +  coord.x  +  "/"  +  coord.y  +  ".png?token=_TOKEN_ID_";  
+                  },  
+                  name:  "Air  Quality",  
+        });  
+  
+      map.overlayMapTypes.insertAt(0,waqiMapOverlay);  
+    }
