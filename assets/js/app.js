88e9936c1400c404d67216de68c5a037922b2562
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-04 23:51:47 -0800

    dynamically creates cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,16 +14,11 @@ var uluru;
 var parks =[];
 var state;
 var stateCode; 
-var doneBool = true;
+var initialLoad = true;
 
 $(document).ready(function(){
-  if (doneBool) {
-    console.log("Ready");
-    onPageLoad();
-    doneBool = false;
-  }
+  onPageLoad();
 });
-
 
 $('#state').on("change", function(event){ 
   event.preventDefault();
@@ -76,11 +71,17 @@ function mainMapInit(parks, div, zooom, center){
 
 
 function onPageLoad(){
+  $("#parks-indicators").empty();
+
   $.ajax({
     url: 'https://developer.nps.gov/api/v1/parks',
     dataType: 'json',
-    data: { stateCode : stateCode, api_key:'dR9liF6s3ztufwHduTKv4mfNqrtq3iGWp8dxjzcr' }
+    data: { 
+      stateCode : stateCode, 
+      fields: "images",
+      api_key:'dR9liF6s3ztufwHduTKv4mfNqrtq3iGWp8dxjzcr'}
   }).done(function(response) {
+    $("#parks-items").empty();
     console.log("Finished ajax call");
 
     waqiMapInit();
@@ -94,6 +95,32 @@ function onPageLoad(){
         uluru = getLatLngFromString(results[i].latLong);               
         parks.push([results[i].fullName, uluru]);
       }
+
+      // var listItem = $('<li data-target="#carouselExampleIndicators" data-slide-to="'+i+'"></li>');
+
+      var fullName = results[i]["fullName"];
+      var description = results[i]["description"];
+      var url = results[i]["url"];
+
+      if (results[i]["images"].length === 0) {
+        console.log("image and caption not available :( ");
+        imgCap = "Error";
+        imgSrc = "https://www.makeupgeek.com/content/wp-content/themes/makeup-geek/images/placeholder-square.svg";
+      } else {
+        var imgSrc = results[i]["images"][0]["url"];
+        var imgCap = results[i]["images"][0]["altText"];
+      }
+
+      // doesn't create the first time when the page loads
+      if (initialLoad === false) {
+        createCards(fullName, description, imgSrc, imgCap, url);
+      }
+
+      // When we reach the end of the first loop, change the bool to false to create cards the next time around
+      if ((i+1) === results.length) {
+        initialLoad = false;
+      }
+      // $("#parks-indicators").append(listItem);
     }
 
     for (var i = 0; i < parks.length; i++) {
@@ -110,22 +137,28 @@ function onPageLoad(){
   });
 }
 
+function createCards(fullName, description, imgSrc, imgCap, url) {
+  // var divToCreate = $('<div class="carousel-item"><img class="d-block w-100" src="'+imgSrc+'" alt="'+imgCap+'"><div class="carousel-caption d-none d-md-block"><h5>'+fullName+'</h5><p>'+description+'</p></div></div>');
+  var divToCreate = $('<div class="card" style="width: 30rem;"><img class="card-img-top" src="'+imgSrc+'" alt="'+imgCap+'"><div class="card-body"><h5 class="card-title">'+fullName+'</h5><p class="card-text">'+description+'</p><a href="'+url+'" class="btn btn-primary">Go somewhere</a></div></div>');
+
+  $("#parks-items").append(divToCreate);
+}
+
 function getLatLngFromString(ll) {
   var newstr = ll.replace(/lat/, '"lat"').replace(/long/i, '"lng"');
   return JSON.parse("{"+newstr+"}"); 
 }
 
 function waqiMapInit(){
-  var  map  =  new  google.maps.Map(document.getElementById('map2'),  {  
-    center:  new  google.maps.LatLng( state),  
-    mapTypeId:  google.maps.MapTypeId.ROADMAP,  
-    zoom:  6  
+  var map = new google.maps.Map(document.getElementById('map2'),  {  
+    center: new google.maps.LatLng( state),  
+    mapTypeId: google.maps.MapTypeId.ROADMAP,  
+    zoom: 6  
   });  
   
-  var  t  =  new  Date().getTime();  
-  var  waqiMapOverlay  =  new  google.maps.ImageMapType({  
-      getTileUrl:  function(coord,  zoom)  {  
-      return  'https://tiles.waqi.info/tiles/usepa-aqi/'  +  zoom  +  "/"  +  coord.x  +  "/"  +  coord.y  +  ".png?token=_TOKEN_ID_"; },  name:  "Air  Quality",  
-  });  
+  var t = new Date().getTime();  
+  var waqiMapOverlay = new google.maps.ImageMapType({  
+      getTileUrl: function(coord,  zoom)  {  
+      return 'https://tiles.waqi.info/tiles/usepa-aqi/'  +  zoom  +  "/"  +  coord.x  +  "/"  +  coord.y  +  ".png?token=_TOKEN_ID_"; },  name:  "Air  Quality" });  
   map.overlayMapTypes.insertAt(0,waqiMapOverlay);  
 }
