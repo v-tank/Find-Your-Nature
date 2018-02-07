33b45dcd4b9f573eedd83b15344e645e2b596efd
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-06 20:00:24 -0800

    added form validation and firebase

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,39 +1,62 @@
+// Firebase
+var config = {
+    apiKey: "AIzaSyC9lq8l9sXlpUwQrGAWUda2qw_-faJUbiA",
+    authDomain: "commentform-807d4.firebaseapp.com",
+    databaseURL: "https://commentform-807d4.firebaseio.com",
+    projectId: "commentform-807d4",
+    storageBucket: "",
+    messagingSenderId: "184726345923"
+  };
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $(document).ready(function(){
   // h3 animation
   setTimeout(function(){$("#in-out").attr("class", "animated tada");}, 1000);
-
-  onPageLoad();
 });
 
+// On click function for submit button on the contact form
 $("#contactSub").on("click", function(event) {
 	event.preventDefault();
 
-	var name = $("#name-input").val();
-	var email = $("#email-input").val();
-	var message = $("#message-input").val();
-	console.log(name + email + message);
+	// save values into variables
+	var name = $("#name-input").val().trim();
+	var email = $("#email-input").val().trim();
+	var message = $("#message-input").val().trim();
 
-	var emailBool = email.includes("@");
+	// if data entered is valid, save it into an array and push the data to a firebase directory
+	if ((name !== "") && (email !== "") && (email.includes("@")) && (message !== "")) {
 
-	if (name.length > 1) {
-		console.log("name, true")
-	}else {
-		console.log("name, false")
-		$("#name-input").val("");
-		$("#name-input").attr("placeholder", "Enter a real name!")
+		var contactFormEntry = {
+			contactName: name,
+			contactEmail: email,
+			contactMessage: message
+		};
+
+		database.ref("/messages").push(contactFormEntry);
+
+		console.log("everything looks good!");
+
+		// empty out the text fields
+		name = $("#name-input").val("");
+		email = $("#email-input").val("");
+		message = $("#message-input").val("");
+	}
+	// if data is invalid, check to see which condition is met and display error accordingly; does not push data to firebase
+	else {
+		if (name === "") {
+			$("#name-input").val("");
+			$("#name-input").attr("placeholder", "Please enter your name!");
+		}
+		if ((email === "") || (!email.includes("@"))) {
+			$("#email-input").val("");
+			$("#email-input").attr("placeholder", "Please enter a valid email!");
+		}
+		if (message === "") {
+			$("#message-input").val("");
+			$("#message-input").attr("placeholder", "Please enter a message!");
+		}
 	}
 
-	if (emailBool == true) {
-		console.log("email, true")
-	}else {
-		console.log("email, false")
-		$("#email-input").val("");
-		$("#email-input").attr("placeholder", "Enter a real email!")
-	}
-	if (message.length < 2) {
-		console.log("message, too short")
-		$("#message-input").val("");
-		$("#message-input").attr("placeholder", "Enter a real message!")
-	}
-
-})
+});
